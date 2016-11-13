7ad482b8b0eb2a06acf4544efe0e2470b9a97c19
Megaju <julen_malle@outlook.fr>
2016-11-13 17:06:16 +0100

    anim book ok

--- js/script.js
+++ js/script.js
@@ -32,4 +32,161 @@
                 } 
             });
         };
+        
+        // BOOK SECTION
+        // book n°1
+        $('#anim-book-0').css({
+            "transition":".8s",
+            "position":"relative",    
+            "top":"400px",    
+            "opacity":"0"    
+        });
+        $('#book-img-0').css({
+            "transition":".8s",
+            "position":"relative",    
+            "left":"100%",    
+            "opacity":"0"    
+        });
+        $('#book-n-0').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#anim-book-0').css({  
+                    "top":"0",    
+                    "opacity":"1"    
+                });
+            } 
+        });
+        $('#book-n-0').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#book-img-0').css({  
+                    "left":"0",    
+                    "opacity":"1"    
+                });
+                $('#book-n-0').off('inview');
+            } 
+        });
+        
+        // book n°2
+        $('#anim-book-1').css({
+            "transition":".8s",
+            "position":"relative",    
+            "top":"400px",    
+            "opacity":"0"    
+        });
+        $('#book-img-1').css({
+            "transition":".8s",
+            "position":"relative",    
+            "left":"100%",    
+            "opacity":"0"    
+        });
+        $('#book-n-1').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#anim-book-1').css({  
+                    "top":"0",    
+                    "opacity":"1"    
+                });
+            } 
+        });
+        $('#book-n-1').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#book-img-1').css({  
+                    "left":"0",    
+                    "opacity":"1"    
+                });
+            } 
+            $('#book-n-1').off('inview');
+        });
+        
+        // book n°3
+        $('#anim-book-2').css({
+                "transition":".8s",
+                "position":"relative",    
+                "top":"400px",    
+                "opacity":"0"    
+            });
+            $('#book-img-2').css({
+                "transition":".8s",
+                "position":"relative",    
+                "left":"100%",    
+                "opacity":"0"    
+            });
+        $('#book-n-2').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#anim-book-2').css({  
+                    "top":"0",    
+                    "opacity":"1"    
+                });
+            } 
+        });
+        $('#book-n-2').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#book-img-2').css({  
+                    "left":"0",    
+                    "opacity":"1"    
+                });
+            } 
+            $('#book-n-2').off('inview');
+        });
+        
+        // book n°4
+        $('#anim-book-3').css({
+                "transition":".8s",
+                "position":"relative",    
+                "top":"400px",    
+                "opacity":"0"    
+            });
+            $('#book-img-3').css({
+                "transition":".8s",
+                "position":"relative",    
+                "left":"100%",    
+                "opacity":"0"    
+            });
+        $('#book-n-3').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#anim-book-3').css({  
+                    "top":"0",    
+                    "opacity":"1"    
+                });
+            } 
+        });
+        $('#book-n-3').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#book-img-3').css({  
+                    "left":"0",    
+                    "opacity":"1"    
+                });
+            } 
+            $('#book-n-3').off('inview');
+        });
+        
+        // book n°5
+        $('#anim-book-4').css({
+                "transition":".8s",
+                "position":"relative",    
+                "top":"400px",    
+                "opacity":"0"    
+            });
+            $('#book-img-4').css({
+                "transition":".8s",
+                "position":"relative",    
+                "left":"100%",    
+                "opacity":"0"    
+            });
+        $('#book-n-4').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#anim-book-4').css({  
+                    "top":"0",    
+                    "opacity":"1"    
+                });
+            } 
+        });
+        $('#book-n-4').on('inview', function(event, isInView) {
+            if (isInView) {
+                $('#book-img-4').css({  
+                    "left":"0",    
+                    "opacity":"1"    
+                });
+            } 
+            $('#book-n-4').off('inview');
+        });
+        
     });
